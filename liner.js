4546ffa90f2e89aa8caa9dfc28252626f03d2ce5
Andrew Beresford <beezly@beezly.org.uk>
2016-03-09 22:27:44 +0000

    Add permissions around Tasks collection

--- liner.js
+++ liner.js
@@ -1,5 +1,32 @@
 Tasks = new Ground.Collection("tasks");
 
+Tasks.allow({
+  insert: function (userId, doc) {
+    return (userId && doc.owner === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    // can only change your own documents
+    return doc.owner === userId;
+  },
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.owner === userId;
+  },
+  fetch: ['owner']
+});
+
+Tasks.deny({
+  update: function (userId, doc, fields, modifier) {
+    // can't change owners
+    return _.contains(fields, 'owner');
+  },
+  remove: function (userId, doc) {
+    // can't remove locked documents
+    return false; 
+  },
+  fetch: ['locked'] // no need to fetch 'owner'
+});
+
 if (Meteor.isServer) {
   // This code only runs on the server
   Meteor.publish("tasks", function () {
